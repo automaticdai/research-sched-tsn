606edaa9a3be0287f377e9440cfd76134377e084
automaticdai <automatic.dai@gmail.com>
2019-10-30 15:12:50 +0000

    minor changes

--- psoobj.m
+++ psoobj.m
@@ -1,13 +1,12 @@
-function tsss = psoobj(xy)
-    tsss = [];
+function tss_arr = psoobj(xy)
     m_size = size(xy);
+    tss_arr = zeros(m_size(1),1);
     
     for i = 1:m_size(1)
        x = -1 * xy(i,1);
        y = xy(i,2);
        poles = [x + y * 1i   x - y * 1i];
        tss = dcdesigner(poles);
-       tsss = [tsss;tss];
+       tss_arr(i) = tss;
     end
-    
 end
