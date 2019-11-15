27ea6b1a9f033e542457869b6ef06b24ee8346c4
automaticdai <automatic.dai@gmail.com>
2019-11-15 22:07:53 +0000

    improved LTI simulator and main.m

--- ltisim_d.m
+++ ltisim_d.m
@@ -18,20 +18,23 @@ function [t, x, u] = ltisim_d(x0, Ad, Bd, K, Ns, Ts)
 % Testset:
 % [t,x,u] = ltisim_d(10, 0.5, 0.3, 1, 100, 0.01)
 
-x = zeros(1, Ns);
-u = zeros(1, Ns);
-t = zeros(1, Ns);
+num_of_states = size(x0,1);
+num_of_inputs = size(Bd,1);
+
+t = zeros(Ns, 1);
+x = zeros(Ns, num_of_states);
+u = zeros(Ns, num_of_inputs);
 
 % i = 1
 t(1) = 0;
-x(1) = x0;
-u(1) = x0 * K;
+x(1,:) = x0;
+u(1,:) = K * x0;
 
 % i >= 2
 for i = 2:Ns
     t(i) = t(i - 1) + Ts;
-    x(i) = Ad .* x(i-1) + Bd .* u(i-1);
-    u(i) = x(i) .* K;
+    x(i,:) = Ad * x(i-1,:)' + Bd * u(i-1);
+    u(i) = K * x(i,:)';
 end
 
 end
--- main.m
+++ main.m
@@ -1,29 +1,56 @@
+%% Define Dataset
 % [C_i, T_i, D_i, P_i, J_i]
 % read a taskset
 h = analysis.rta;
 
 taskset_arr = [1 2 3 1 0; 2 2 3 2 0];
 
+
+%% Test Schedulability
 % run GA
 
-    % check schedulability
-    bSched = h.schedulabilityTest(taskset_arr);
-    disp(bSched)
-    
-    % run PSO
-    % generate population
-    % check input constraint
-    % update the speed
+% check schedulability
+bSched = h.schedulabilityTest(taskset_arr);
+disp(bSched);
+
+% run PSO
+% generate population
+% check input constraint
+% update the speed
     
 %bSched = h.schedulabilityTest(taskset);
 
-%%
-[t,x,u] = ltisim_d([0 10], [0.5 0.2], 0.3, [0.25;0.4], 100, 0.01);
 
-% plot results
+%% Run LTI simulation
+x0 = [0; 10];
+Ad = [0.8 0.15; 0.4 0.35];
+Bd = 0.35;
+K = [0.15 0.05];
+Ns = 200;
+Ts = 0.01;
+
+Acl = Ad + Bd * K;
+
+eig(Acl)
+
+if (max(abs(eig(Acl))) >= 1)
+    disp("Unstable system!")
+end
+
+[t,x,u] = ltisim_d(x0, Ad, Bd, K, Ns, Ts);
+
+
+%% plot results
 subplot(2,1,1);
-plot(t, x);
+for i = 1:size(x0,1)
+    plot(t, x(:,i));
+    hold on;
+end
+ylabel("States: x_k")
 subplot(2,1,2);
 plot(t, u);
+ylabel("Intpus: u_k")
 
-% save results
+
+%% Save results
+%save()
