9a4c256d0e75e33fc42e2855a713a36e8f11c902
automaticdai <automatic.dai@gmail.com>
2019-11-20 19:57:38 +0000

    added test case of taskset_gen

---
+++ tasksets/taskset_gen_test.m
@@ -0,0 +1,33 @@
+format long g
+
+% Parameters
+N = 20;                        % number of tasks
+U_bar = 0.8;                   % desired utilization
+
+taskset = taskset_gen(N, U_bar);
+
+% print result
+Ci = taskset(:,1);
+Ti = taskset(:,2);
+A = sym(Ti);
+LCM = lcm(A)
+
+fprintf('\r Generated Taskset (Ci, Ti, Di, Pi, Ji): \r\r');
+disp(taskset);
+
+fprintf('The actual task total utilization is: %0.3f \r', sum(Ci ./ Ti));
+
+% schedulability check
+bSched = rta.schedulabilityTest(taskset);
+Response_Time = rta.ResponseTimeAnalysis(taskset)
+
+if (bSched)
+    disp("Packet set is schedulable.")
+else
+    disp("Error: Packet set is unschedulable.")
+end
+
+% save dataset
+%filename = sprintf("taskset_u_%0.2f.mat", U_bar);
+%taskset_nc = taskset;
+%save(filename, 'taskset_nc');
