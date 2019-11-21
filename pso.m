79f085c995d6488bffef4350616a79ed1065e2e0
automaticdai <automatic.dai@gmail.com>
2019-11-21 15:39:53 +0000

    refined PSO

--- pso.m
+++ pso.m
@@ -1,39 +1,41 @@
-% PSO
-% This function is written as a simple example of PSO
-% in order to optimiza other functions you have to change the objective
-% function.
-%
-% to increase the efficieny of the PSO this version is a vectorized version
+function [tss_best, x1_best, x2_best] = pso()
+% -----------------------------------
+% PSO Control Poles Optimization
+% This function is written using PSO to solve control poles.
+% Current version only support second-order systems!
+% 
+% To increase the efficieny of the PSO this version is a vectorized version
 % for MATLAB.
 %
 % -----------------------------------
-% Xiaotian Dai
-% credit to: REZA AHMADZADEH  (based on the original code by Wesam Elshamy)
+% Xiaotian Dai, University of York
+% Credit to: Reza Ahmadzadeh
 % -----------------------------------
-clc; clear; close all;
 
 %% initialization
-%
-swarm_size = 64;                       % number of the swarm particles
-maxIter = 50;                          % maximum number of iterations
+swarm_size = 64;                        % number of the swarm particles
+maxIter = 50;                           % maximum number of iterations
 inertia = 1.0;
 correction_factor = 2.0;
+
 % set the position of the initial swarm
-a = 1:8;
-[X,Y] = meshgrid(a,a);
-C = cat(2,X',Y');
+a = 0:7;
+b = 0:7;
+[X,Y] = meshgrid(a,b);
+C = cat(2, X', Y');
 D = reshape(C,[],2);
 swarm(1:swarm_size,1,1:2) = D;          % set the position of the particles in 2D
 swarm(:,2,:) = 0;                       % set initial velocity for particles
 swarm(:,4,1) = 1000;                    % set the best value so far
-plotObjFcn = 1;                         % set to zero if you do not need a final plot
+b_plot = 1;                             % set to zero if you do not need a final plot
+
 
 %% The main loop of PSO
-tic;
+%tic;
 
 for iter = 1:maxIter
-    swarm(:, 1, 1) = swarm(:, 1, 1) + swarm(:, 2, 1)/1.3;       %update x position with the velocity
-    swarm(:, 1, 2) = swarm(:, 1, 2) + swarm(:, 2, 2)/1.3;       %update y position with the velocity
+    swarm(:, 1, 1) = swarm(:, 1, 1) + swarm(:, 2, 1)/1.3;       % update x position with the velocity
+    swarm(:, 1, 2) = swarm(:, 1, 2) + swarm(:, 2, 2)/1.3;       % update y position with the velocity
     x = swarm(:, 1, 1);                                         % get the updated position
     y = swarm(:, 1, 2);                                         % updated position
     fval = pso_obj_func([x y]);                                 % evaluate the function using the position of the particle
@@ -47,7 +49,7 @@ for iter = 1:maxIter
         end
     end
     
-    [~, gbest] = min(swarm(:, 4, 1));                           % find the best function value in total
+    [~, gbest] = min(swarm(:, 4, 1));                           % find the best function value (minimal) in total
     
     % update the velocity of the particles
     swarm(:, 2, 1) = inertia*(rand(swarm_size,1).*swarm(:, 2, 1)) + correction_factor*(rand(swarm_size,1).*(swarm(:, 3, 1) ...
@@ -56,23 +58,19 @@ for iter = 1:maxIter
         - swarm(:, 1, 2))) + correction_factor*(rand(swarm_size,1).*(swarm(gbest, 3, 2) - swarm(:, 1, 2)));   %y velocity component
     
     % plot the particles
-    clf;plot(swarm(:, 1, 1), swarm(:, 1, 2), 'bx');             % drawing swarm movements
-    axis([-2 40 -2 40]);
-    pause(.1);                                                 % un-comment this line to decrease the animation speed
-    disp(['iteration: ' num2str(iter)]);
+    if b_plot
+        clf;plot(swarm(:, 1, 1), swarm(:, 1, 2), 'bx');             % drawing swarm movements
+        axis([-10 10 -10 10]);
+        pause(.1);                                                  % un-comment this line to decrease the animation speed
+    end
+    
+    tss_best = swarm(gbest, 4, 1);
+    x1_best = swarm(gbest, 1, 1);
+    x2_best = swarm(gbest, 1, 2);
+    
+    disp(['iteration: ' num2str(iter) ', best: ' num2str(tss_best)]);
 end
 
-toc
+%toc
 
-%% plot the function
-% if plotObjFcn
-%     ub = 40;
-%     lb = 0;
-%     npoints = 1000;
-%     x = (ub-lb) .* rand(npoints,2) + lb;
-%     for ii = 1:npoints
-%         f = objfcn([x(ii,1) x(ii,2)]);
-%         plot3(x(ii,1),x(ii,2),f,'.r');hold on
-%     end
-%     plot3(swarm(1,3,1),swarm(1,3,2),swarm(1,4,1),'xb','linewidth',5,'Markersize',5);grid
-% end
+end
--- pso_obj_func.m
+++ pso_obj_func.m
@@ -1,12 +1,15 @@
 % Objective function for PSO
-function obj_arr = psoobj(xy)
+function obj_arr = pso_obj_func(xy)
     m_size = size(xy);
     obj_arr = zeros(m_size(1),1);
     
     for i = 1:m_size(1)
-       x = -1 * xy(i,1);
-       y = xy(i,2);
-       poles = [x + y * 1i   x - y * 1i];
+       x1 = xy(i,1);
+       x2 = xy(i,2);
+       if (x2 == 0)
+            x2 = x2 + 1e-6;
+       end
+       poles = [x1 + x2*1i   x1 - x2*1i];
        tss = dcdesigner(poles);
        obj_arr(i) = tss;
     end
