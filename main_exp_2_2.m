ffff261bdf52533ea998ef0ad1cc7320c2f5a546
automaticdai <automatic.dai@gmail.com>
2019-11-21 15:40:31 +0000

    implemented Exp2_2

--- main_exp_2_2.m
+++ main_exp_2_2.m
@@ -2,90 +2,42 @@
 
 clear; clc;
 
-%% Define Parameters
-% scheduling system
-N = 10;         % number of packets
-U_bar = 0.2;    % desired utilization
+global Ts
+global U_MAX
+global p
 
-%taskset = taskset_gen(N, U_bar);
-%bSched = rta.schedulabilityTest(taskset);
-
-% control system
+%% Parameters
+% control system (continous)
 syss_param = [0.01,0.1,0.01,1,0.5;
               0.10,0.2,0.01,1,0.55;
               0.15,0.3,0.01,1,0.6];
 
-w1 = 0.8;
-w2 = 0.6;
-w3 = 0.4;
+sys_idx = 1;
+p = dc_motor(syss_param(sys_idx,:)); % dynamics
+Ts = 0.001;   % sampling time
+U_MAX = 12; % input constraint
 
-J = 0;
-j1 = 0;
-j2 = 0;
-j3 = 0;
+tss_best_a = zeros(100,1); 
+x1_best_a = zeros(100,1);
+x2_best_a = zeros(100,1);
 
-
-%% Simulate
-% control system (continous)
-x0 = [1; 0];
-
-p = dc_motor(syss_param(1,:));
-A = p.A;
-B = p.B;
-C = p.C;
-D = p.D;
-
-K = [2.165 0.21];
-Ns = 2000;
-dt = 0.001;
-Ts = 0.001;
-
-Acl = A + B * K;
-pcl = ss(Acl,B,C,D);
-
-% control system (discrete)
-
-
-%% check closed-loop stability
-eig(Acl)
-if (max(eig(Acl)) >= 0)
-    disp("[Error] Unstable system!")
-else
-    disp("System is stable.")
+for i = 1:100
+    [tss_best, x1_best, x2_best] = pso();
+    tss_best_a(i) = tss_best;
+    x1_best_a(i) = x1_best;
+    x2_best_a(i) = x2_best;
+    Ts = Ts + 0.001;
 end
 
-
-%% Run LTI simulation
-[t, x, u] = ltisim(x0, A, B, K, Ns, dt);
-
-
-% pd = c2d(p, Ts);
-%
-% Ad = pd.A;
-% Bd = pd.B;
-% Cd = pd.C;
-% Dd = pd.D;
+%poles = [-5+0.1622i  -5-0.1622i];
+%tss = dcdesigner(poles);
 
 
-%% Plot results
-% plot states
-subplot(2,1,1);
-for i = 1:size(x,2)
-    stairs(t, x(:,i));
-    hold on;
-end
-ylabel("States: x_k")
-xlabel("t")
-
-% plot inputs
-subplot(2,1,2);
-for i = 1:size(u,2)
-    stairs(t, u(:,i));
-    hold on;
-end
-ylabel("Intpus: u_k")
-xlabel("t")
+%%
+% for sys_i = 1:size(syss_param,1)
+%     p = dc_motor(syss_param(sys_i,:));
+% end
 
 
 %% Save results
-%save()
+save("result2-2.mat","tss_best_a","x1_best_a","x2_best_a")
