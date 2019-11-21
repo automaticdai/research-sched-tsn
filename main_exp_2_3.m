3a6dd09698668cd5a51e009b0b21f11289dadb22
automaticdai <automatic.dai@gmail.com>
2019-11-21 21:51:10 +0000

    added Exp_2_3

--- main_exp_2_3.m
+++ main_exp_2_3.m
@@ -1,3 +1,21 @@
 % Searching the maximum value
 
 clear; clc;
+
+addpath('tasksets')
+addpath('plants')
+addpath('data')
+addpath('rta')
+
+%% Parameters
+w1 = 0.5;
+w2 = 0.3;
+w3 = 0.2;
+
+j1 = 0;
+j2 = 0;
+j3 = 0;
+
+
+
+J = w1 * j1 + w2 * j2 + w3 * j3;
