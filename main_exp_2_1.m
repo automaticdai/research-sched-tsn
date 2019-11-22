3aa866f1fcddcc32c7e3bb50ed11b2d29c417627
automaticdai <automatic.dai@gmail.com>
2019-11-22 10:54:27 +0000

    make Exp 2_1 multiple loops

--- main_exp_2_1.m
+++ main_exp_2_1.m
@@ -7,16 +7,19 @@ addpath('plants')
 addpath('data')
 addpath('rta')
 
-spmd
-    javaaddpath(strcat(pwd(),'/rta/rta.jar'));
-end
+javaaddpath(strcat(pwd(),'/rta/rta.jar'));
+rta = analysis.RTA;
+
+%spmd   
+%javaaddpath(strcat(pwd(),'/rta/rta.jar'));
+%end
 
 %% Define Parameters
 % scheduling system
-N = 10;         % number of packets
-U_bar = 0.9;    % desired utilization
+N = 10;                           % number of packets
+U_bar = [0.5 0.6 0.7 0.8 0.9];    % desired utilization
 
-N_c = 3;        % number of control packets
+N_c = 3;                          % number of control packets
 
 search_range = [1 100] * 1000;
 search_step = 1000;
@@ -26,40 +29,49 @@ search_numel = numel(search_space);
 
 %% this is one experiment
 % generate a taskset
-taskset_nc = taskset_gen(N, U_bar);
-candidate_solutions = zeros(search_numel,search_numel,search_numel);
 
-parfor i = 1:search_numel
-    for j = 1:search_numel
-        for k = 1:search_numel
-            % add control packets 
-            Ti = search_space(i);
-            taskset_c1 = [120, Ti, Ti, -1, 2];
-            
-            Tj = search_space(j);
-            taskset_c2 = [120, Tj, Tj, -1, 2];           
-            
-            Tk = search_space(k);
-            taskset_c3 = [120, Tk, Tk, -1, 2];
-            
-            taskset = [taskset_nc; taskset_c1; taskset_c2; taskset_c3];
+for kk = 0:1000
+    U_bar_this = U_bar(mod(kk,5) + 1);
+    
+    disp([kk U_bar_this])
+    
+    taskset_nc = taskset_gen(N, U_bar_this);
+    candidate_solutions = zeros(search_numel,search_numel,search_numel);
 
-            % reorder priorities by deadlines
-            taskset = sortrows(taskset, 3, 'ascend');
-            Pi = ((N-1+N_c):-1:0)';
-            taskset(:,4) = Pi;
+    for i = 1:search_numel
+        for j = 1:search_numel
+            for k = 1:search_numel 
+                % add control packets 
+                Ti = search_space(i);
+                taskset_c1 = [120, Ti, Ti, -1, 2];
 
-            % test schedulability
-            rta = analysis.RTA;
-            bSched = rta.schedulabilityTest(taskset);
+                Tj = search_space(j);
+                taskset_c2 = [120, Tj, Tj, -1, 2];           
 
-            % if scheduable, add as a candidate solution
-            if bSched
-                candidate_solutions(i,j,k) = 1;
+                Tk = search_space(k);
+                taskset_c3 = [120, Tk, Tk, -1, 2];
+
+                taskset = [taskset_nc; taskset_c1; taskset_c2; taskset_c3];
+
+                % reorder priorities by deadlines
+                taskset = sortrows(taskset, 3, 'ascend');
+                Pi = ((N-1+N_c):-1:0)';
+                taskset(:,4) = Pi;            
+
+                % test schedulability
+                bSched = rta.schedulabilityTest(taskset);
+
+                % if scheduable, add as a candidate solution
+                if bSched
+                    candidate_solutions(i,j,k) = 1;
+                end
             end
         end
-        disp("Progress:" + ((i - 1) * search_numel + j) / (search_numel^2) * 100 + "%")
+        disp("Progress:" + (i) / (search_numel) * 100 + "%")
     end
-end
 
-save("result.mat","candidate_solutions")
+    % save file
+    filename_str = sprintf('./data/s_%0.2f_%d_%d.mat', U_bar_this, N, ceil(kk / 5 + 0.1));
+    save(filename_str,'candidate_solutions')
+
+end
