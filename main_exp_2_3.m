613c4ff6dff16de98802e88465a32234b3448b71
automaticdai <automatic.dai@gmail.com>
2019-11-23 14:43:33 +0000

    finalize exp 2_3

--- main_exp_2_3.m
+++ main_exp_2_3.m
@@ -10,6 +10,7 @@ addpath('rta')
 
 %% Parameters
 T_list = [0.5, 1, 2, 5, 10, 20, 50, 100, 200] * 10;
+U_bar = [0.70 0.75 0.80 0.85 0.90 0.95];
 
 % controller weightings
 w1 = 0.5;
@@ -31,55 +32,88 @@ load("data\v_p3_ts.mat")
 v3 = tss_best_a;
 v3n = ((v3 - min(v3)) ./ (max(v3) - min(v3)));
 
-%plot([v1n v2n v3n])
-
-
-%!!!!!!!!!!!!!!!!!!
-v1n = v1;
-v2n = v2;
-v3n = v3;
-
 
 %% Main
+S_f_a = [];
+S_f_a_idx = [];
+n_f_a = [];
+n_nf_a = [];
+J_avg_a = [];
+
 % Open a feasible candidates
-U_bar_this = 0.9;
-N = 10;
-J_sum = 0;
+for u_bar_idx = 1:6
+    U_bar_this = U_bar(u_bar_idx);
+    N = 10;
+    J_sum = 0;
 
-for kk = 1:200
-    filename_str = sprintf('./data/s_%0.2f_%d_%d.mat', U_bar_this, N, kk);
-    load(filename_str)
-    S = candidate_solutions;
+    n_f = 0;    % # of feasible solutions
+    S_f = [];   % list of feasible solutions
+    n_nf = 0;   % # of infeasible solutions
 
-    s_hat = [];
-    J_hat = 100;
-    
-    for i = 1:9
-        for j = 1:9
-            for k = 1:9
-                if S(i,j,k)
-                    j1 = v1n(i);
-                    j2 = v2n(j);
-                    j3 = v3n(k);
+    for kk = 1:1000
+        filename_str = sprintf('./data/s_%0.2f_%d_%d.mat', U_bar_this, N, kk);
+        load(filename_str)
+        S = candidate_solutions;
 
-                    J = w1 * j1 + w2 * j2 + w3 * j3;
+        s_hat = [];
+        J_hat = 100;
 
-                    if (J < J_hat)
-                        s_hat = [J, i, j, k];
-                        J_hat = J;
+        for i = 1:9
+            for j = 1:9
+                for k = 1:9
+                    if S(i,j,k)
+                        j1 = v1n(i);
+                        j2 = v2n(j);
+                        j3 = v3n(k);
+
+                        J = w1 * j1 + w2 * j2 + w3 * j3;
+
+                        if (J < J_hat)
+                            s_hat = [J, i, j, k];
+                            J_hat = J;
+                        end
                     end
                 end
             end
         end
+
+        if J_hat ~= 100
+            fprintf('%d: %0.3f %d %d %d \n', kk, s_hat(1), s_hat(2), s_hat(3), s_hat(4));
+            J_sum = J_sum + s_hat(1);
+            n_f = n_f + 1;
+            S_f = [S_f; s_hat(1)];
+        else
+            fprintf('%d: [] \n', kk);
+            n_nf = n_nf + 1;
+        end
+
     end
 
-    if J_hat ~= 100
-        fprintf('%d: %0.3f %d %d %d \n', kk, s_hat(1), s_hat(2), s_hat(3), s_hat(4));
-        J_sum = J_sum + J;
-    else
-        fprintf('%d: [] \n', kk);
-    end
-
+    J_avg = J_sum / n_f;
+    fprintf('%f, %d, %d \n', J_avg, n_f, n_nf);
+    
+    J_avg_a = [J_avg_a; J_avg];
+    n_f_a = [n_f_a; n_f];
+    n_nf_a = [n_nf_a; n_nf];
+    S_f_a = [S_f_a; S_f];
+    S_f_a_idx = [S_f_a_idx; ones(n_f, 1) * U_bar_this];
+    
 end
 
-J_sum / 200
+
+%% plot results
+close all;
+figure()
+boxplot(S_f_a, S_f_a_idx, 'whisker',2)
+xlabel("Network Load")
+ylabel("Control Cost (normalized)")
+
+figure()
+plot(U_bar, J_avg_a, 'rd-.')
+xlabel("Network Load")
+ylabel("Average Control Cost (normalized)")
+
+figure()
+plot(U_bar, n_f_a / 1000 * 100, 'bd-.')
+xlabel("Network Load")
+ylabel("Schedule Packet Sets (precentage)")
